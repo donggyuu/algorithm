fb1fb49c072eac223dba74f6d1200613b55a3305
donggyuu <donggyu.rhee@gmail.com>
2020-07-07 19:50:16 +0900

    REFACTORING meeting room problem

--- algorithm-java/src/MeetingRoom.java
+++ algorithm-java/src/MeetingRoom.java
@@ -2,9 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// TODO : delete "static"
-// TODO : make solution using array(not list)
-
 public class MeetingRoom {
 
     /**
@@ -29,49 +26,39 @@ public class MeetingRoom {
 
     public static void main(String[] args) {
 
+        // ==== solution ====
         // 회의 시작시간 기준 오름차순 정렬
+        // Sort by ascend order by MeetingStartTime
+
         // n번째 회의가 끝나는 시간 < n+1번째 회의가 시작하는 시간.
+        // End time of "n" < Start time of n+1
+        // ===================
 
-        // 배열을 입력받는다
-        // 첫 것을 빼서 오름차순으로 재정렬.
-        // for를 돌면서, n의 1번쨰, n+1의 0번째를 비교한다.
-
-
+        // dummy data
         List<Interval> intervals = new ArrayList<Interval>();
         intervals.add(new Interval(2, 4));
         intervals.add(new Interval(7, 10));
         intervals.add(new Interval(11, 20));
 
-        // another way - use array
-        // Interval[] intervals = {interval_1, interval_2, interval_3};
-
-        List<Interval> sortedIntervalsByStartTime = intervals.stream()
+        List<Interval> intervalsSortedByStartTime = intervals.stream()
                 .sorted((x, y) -> x.getStart() - y.getStart())
                 .collect(Collectors.toList());
 
-        // check sorted list
-        for (int i=0; i<3; i++) {
-            System.out.println(sortedIntervalsByStartTime.get(i).getStart());
-        }
-
-        // solution
-        System.out.println(solution(sortedIntervalsByStartTime));
+        System.out.println(solution(intervalsSortedByStartTime));
 
     }
 
-
     public static boolean solution(List<Interval> sortedIntervalsByStartTime) {
 
-        // for를 돌면서, n의 1번쨰, n+1의 0번째를 비교한다.
-        for (int i=0; i<2; i++) {
+        int nMeeting = sortedIntervalsByStartTime.size();
+        for (int i = 0; i < nMeeting - 1; i++) {
 
-            int endValueOfN = sortedIntervalsByStartTime.get(i).getEnd();
-            int startValueofNplusOne = sortedIntervalsByStartTime.get(i + 1).getStart();
+            int endTimeBeforeMeeting = sortedIntervalsByStartTime.get(i).getEnd();
+            int startTimeNextMeeting = sortedIntervalsByStartTime.get(i + 1).getStart();
 
-            if (endValueOfN >= startValueofNplusOne) {
+            if (endTimeBeforeMeeting >= startTimeNextMeeting) {
                 return false;
             }
-
         }
 
         return true;
@@ -82,18 +69,11 @@ public class MeetingRoom {
         int start;
         int end;
 
-//        // 지정 안해도 어차피 0으로 초기화됨
-//        Interval() {
-//            this.start = 0;
-//            this.end = 0;
-//        }
-
         Interval(int start, int end) {
             this.start = start;
             this.end = end;
         }
 
-        // Getter
         public int getStart() {
             return start;
         }
@@ -103,6 +83,5 @@ public class MeetingRoom {
         }
 
     }
-
 
 }
